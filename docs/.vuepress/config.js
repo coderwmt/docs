69eb21c3abd486cd62250dad99605de208d27f32
corderwmt <806404094@qq.com>
2021-11-28 16:55:38 +0800

    feat:pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,30 @@
 module.exports = {
-  base:"/docs/",
+  base: "/docs/",
   title: "WuSir",
-  description:"关于我学习前端的个人博客",
-  head:[
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  description: "关于我学习前端的个人博客",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     // ["meta",{ name:"authour",content:"WuSir"}],
     // ["meta",{ name:"keywords",content:"vuepress,前端开发"}]
+  ],
+  plugins: [
+    [
+      '@vuepress/pwa', {
+        serviceWorker: true,
+        updatePopup: {
+          message: "发现新内容可用",
+          buttonText: "刷新"
+        }
+      }
+    ]
   ],
   themeConfig: {
     lastUpdated: '更新时间',
@@ -30,9 +49,9 @@ module.exports = {
         'css-c'   /* /css/css-c.html */
       ],
       '/javascript/': [
-        'javascript-a',  
-        'javascript-b',  
-        'javascript-c'   
+        'javascript-a',
+        'javascript-b',
+        'javascript-c'
       ]
     }
   }
