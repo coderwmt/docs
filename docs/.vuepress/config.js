019e08c1c9f4901e89c2c0b57ed2dcce55e18e20
corderwmt <806404094@qq.com>
2021-11-28 20:11:37 +0800

    feat:backtop

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -30,7 +30,8 @@ module.exports = {
       clientId:"573ddd11cba4657cfcfb",
       clientSecret:"90568dc2bfaaca7290368962931a072753310e51",
       autoCreateIssue:true
-    }
+    },
+    '@vuepress/back-to-top':true
   },
   themeConfig: {
     lastUpdated: '更新时间',
