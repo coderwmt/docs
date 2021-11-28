c0dc4c505b1a0f0e2af9f90cc8a13515947f7062
corderwmt <806404094@qq.com>
2021-11-28 13:41:45 +0800

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# docs
-my own blog
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  title: "WuSir",
+  description:"关于我学习前端的个人博客",
+  head:[
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    // ["meta",{ name:"authour",content:"WuSir"}],
+    // ["meta",{ name:"keywords",content:"vuepress,前端开发"}]
+  ],
+  themeConfig: {
+    lastUpdated: '更新时间',
+    logo: '/assets/img/hero.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/about/' },
+      {
+        text: 'Languages',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'Chinese', link: '/language/chinese/' },
+          { text: 'Japanese', link: '/language/japanese/' }
+        ]
+      },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: {
+      '/css/': [
+        'css-a',     /* /css/css-a.html*/
+        'css-b',  /* /css/css-b.html */
+        'css-c'   /* /css/css-c.html */
+      ],
+      '/javascript/': [
+        'javascript-a',  
+        'javascript-b',  
+        'javascript-c'   
+      ]
+    }
+  }
+}
